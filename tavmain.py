de7eeb64a26783c7b1d94e8ae7c6ef0fcec37477
Djeferson Preis <djefersonpreis@gmail.com>
2022-10-03 23:21:31 +0000

    aula 11 - 03-10-2022

--- tavmain.py
+++ tavmain.py
@@ -7,9 +7,19 @@ st.title('App - Tópicos Avançados')
 @st.cache 
 def load_database():
     return pd.read_feather('database/GS.feather'), \
-        pd.read_feather('database/classificacaoz_consumidor.feather')
+        pd.read_feather('database/classificacaoz_consumidor.feather'), \
+        pd.read_feather('database/clusterizacao_pais.feather'), \
+        pd.read_feather('database/regressao_mercado.feather'), \
+        pd.read_feather('database/regressao_regiao.feather'), \
+        pd.read_feather('database/localizacao.feather')
     
-gs, cla_con = load_database()
+gs, cla_con, clu_pai, reg_mer, reg_reg, coords = load_database()
+
+rg_mer = reg_mer.copy()
+rg_mer['ano'] = rg_mer['ds'].dt.year
+
+rg_reg = reg_reg.copy()
+rg_reg['ano'] = rg_reg['ds'].dt.year
 
 # st.dataframe(gs) 
 # st.dataframe(cla_con) 
@@ -37,7 +47,49 @@ with taberp:
     cl4.metric('Quantidade Comprada', round(gs_con['Quantity'].sum(), 2), '1')
     with st.expander('Pedidos:'):
         st.dataframe(gs_con[['Order Date', 'Product Name', 'Quantity', 'Sales', 'Profit']])
+    
+    clu_pai_cli = clu_pai[clu_pai['reference'] == gs_con['Country'].values[0]]
+    st.dataframe(clu_pai_cli[['cluster', 'm_deliver', 'm_profit', 'm_sales', 'm_quantity', 'f_sales', 'f_profit', 'r_days']])
+    st.dataframe(clu_pai_cli[['cluster', 'clm_deliver', 'clm_profit', 'clm_saless', 'clm_quantity', 'clf_sales', 'clf_profit', 'clr_days']])
+
+    ## TODO: MAPA com as coordenadas do cliente
+
+
 with tabbi:
     st.header('Dados do Business Intelligence')
+    with st.expander('Mercado'):
+        aggm = st.selectbox('Agregador Mercado', ['sum', 'mean'])
+        st.dataframe(rg_mer.pivot_table(index='Market', columns='ano', values='yhat', aggfunc=aggm, fill_value=0))
+    
+
+        if st.checkbox('Detalhar mercado'):
+            mercado = st.selectbox('Mercado:', rg_mer['Market'].unique())
+            gr_mer = rg_mer[rg_mer['Market'] == mercado].groupby('ano')['yhat'].sum().reset_index()
+            proj = alt.Chart(gr_mer).mark_line(color='blue').encode(x='ano', y='yhat')
+            gr_gs = gs[gs['Market'] == mercado].groupby('OYear')['Sales'].sum().reset_index()
+            real = alt.Chart(gr_gs).mark_line(color='red').encode(x='OYear', y='Sales')
+            st.altair_chart(proj + real)
+
+    with st.expander('Região'):
+        aggr = st.selectbox('Agregador Região', ['sum', 'mean'])
+        st.dataframe(rg_reg.pivot_table(index='Region', columns='ano', values='yhat', aggfunc=aggr, fill_value=0))
+    
+
+        if st.checkbox('Detalhar Região'):
+            regiao = st.selectbox('Região:', rg_reg['Region'].unique())
+            gr_reg = rg_reg[rg_reg['Region'] == regiao].groupby('ano')['yhat'].sum().reset_index()
+            proj = alt.Chart(gr_reg).mark_line(color='blue').encode(x='ano', y='yhat')
+            gr_gs = gs[gs['Region'] == regiao].groupby('OYear')['Sales'].sum().reset_index()
+            real = alt.Chart(gr_gs).mark_line(color='red').encode(x='OYear', y='Sales')
+            st.altair_chart(proj + real)
+
+    # st.dataframe(reg_mer)
+    # st.dataframe(reg_reg)
+
+
+    ## TODO: MAPA dos mercados, das regiões e dos países 
+
+
 with tabstone:
     st.header('Dados do Comércio Eletrônico')
+    st.dataframe(coords)
